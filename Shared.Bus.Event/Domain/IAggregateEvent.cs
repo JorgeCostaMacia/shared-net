0e2623aab377633b5400a9ccdd14c9eb0e381d4f
JorgeCostaMacia <costamaciajorge@gmail.com>
2023-03-08 09:40:52 +0100

    UPDATE Aggregate constructors for Aggregate Messages UPDATE Propagate new constructor

--- Shared.Bus.Event/Domain/IAggregateEvent.cs
+++ Shared.Bus.Event/Domain/IAggregateEvent.cs
@@ -3,6 +3,8 @@
     public abstract class IAggregateEvent : Message.Domain.IAggregateMessage, IEvent
     {
         public IAggregateEvent(Guid aggregateId, DateTime aggregateOccurredAt) : base(aggregateId, aggregateOccurredAt) { }
+        public IAggregateEvent(DateTime aggregateOccurredAt) : base(aggregateOccurredAt) { }
+        public IAggregateEvent(Guid aggregateId) : base(aggregateId) { }
         public IAggregateEvent() : base() { }
     }
 }
