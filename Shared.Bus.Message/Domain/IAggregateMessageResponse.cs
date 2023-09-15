782a1ad7786b8141e406fa34b4e2d76bedb6b9bb
JorgeCostaMacia <costamaciajorge@gmail.com>
2023-09-15 15:36:29 +0200

    UPDATE Update bus aggregates to add Name that it used to know the queue or procedence name

--- Shared.Bus.Message/Domain/IAggregateMessageResponse.cs
+++ Shared.Bus.Message/Domain/IAggregateMessageResponse.cs
@@ -2,19 +2,17 @@
 
 namespace Shared.Bus.Message.Domain
 {
-    public abstract class IAggregateMessage : IMessage, IAggregate
+    public abstract class IAggregateMessageResponse : IMessageResponse, IAggregate
     {
         public Guid AggregateId { get; }
         public string AggregateName { get; }
         public DateTime AggregateOccurredAt { get; }
 
-        protected IAggregateMessage(Guid aggregateId, string aggregateName, DateTime aggregateOccurredAt)
+        protected IAggregateMessageResponse(Guid aggregateId, string aggregateName, DateTime aggregateOccurredAt)
         {
             AggregateId = aggregateId;
             AggregateName = aggregateName;
             AggregateOccurredAt = aggregateOccurredAt;
         }
-
-        public static string AggregateRoute() => throw new NotImplementedException();
     }
 }
---
+++ Shared.Bus.Query/Domain/IAggregateQueryResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace Shared.Bus.Query.Domain
+{
+    public abstract class IAggregateQueryResponse : Message.Domain.IAggregateMessageResponse, IQuery
+    {
+        protected IAggregateQueryResponse(Guid aggregateId, string aggregateName, DateTime aggregateOccurredAt) : base(aggregateId, aggregateName, aggregateOccurredAt) { }
+        protected IAggregateQueryResponse(string aggregateName) : base(Guid.NewGuid(), aggregateName, DateTime.UtcNow) { }
+    }
+}
