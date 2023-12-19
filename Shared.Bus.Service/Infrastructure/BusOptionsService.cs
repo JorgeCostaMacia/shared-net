f781f9291a240aedc8c6324c946c50121474f468
JorgeCostaMacia <costamaciajorge@gmail.com>
2023-12-19 14:19:16 +0100

    ADD Bus.Service UPDATE Packages

---
+++ Shared.Bus.Service/Infrastructure/BusOptionsService.cs
@@ -0,0 +1,13 @@
+﻿namespace InstitutoBernabeu.TTT.Bus
+{
+    public class BusOptionsService
+    {
+        public string Url { get; set; } = string.Empty;
+        public ushort Port { get; set; } = 0;
+        public string Host { get; set; } = string.Empty;
+        public string User { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public int PrefetchCount { get; set; } = 4;
+        public int ConcurrentMessageLimit { get; set; } = 2;
+    }
+}
