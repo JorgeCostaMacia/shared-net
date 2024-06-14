f163aebdf3501111edb7dc8e241eb596542501d0
JorgeCostaMacia <costamaciajorge@gmail.com>
2024-06-14 23:16:08 +0200

    UPDATE Remove SubscriberId

--- Shared.Bus.Query/Domain/IAggregateQuery.cs
+++ Shared.Bus.Query/Domain/IAggregateQuery.cs
@@ -2,7 +2,6 @@
 
 public abstract class IAggregateQuery : Message.Domain.IAggregateMessage, IQuery
 {
-    protected IAggregateQuery(Guid aggregateId, string aggregateConsumer, Guid aggregateSubscriber, DateTime aggregateOccurredAt) : base(aggregateId, aggregateConsumer, aggregateSubscriber, aggregateOccurredAt) { }
-    protected IAggregateQuery(string aggregateConsumer, Guid aggregateSubscriber) : base(aggregateConsumer, aggregateSubscriber) { }
+    protected IAggregateQuery(Guid aggregateId, string aggregateConsumer, DateTime aggregateOccurredAt) : base(aggregateId, aggregateConsumer, aggregateOccurredAt) { }
     protected IAggregateQuery(string aggregateConsumer) : base(aggregateConsumer) { }
 }
