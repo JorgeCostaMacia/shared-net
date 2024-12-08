82948c9b61530288dd7a5ce6bc34e12f227a7f62
JorgeCostaMacia <costamaciajorge@gmail.com>
2024-12-08 15:10:08 +0100

    UPDATE Compatibility

--- Shared.Data.Persistence/Domain/IDataContext.cs
+++ Shared.Data.Persistence/Domain/IDataContext.cs
@@ -1,5 +1,6 @@
 ﻿using Shared.Context.Domain;
 
-namespace Shared.Data.Persistence.Domain;
-
-public interface IDataContext : IContext { }
+namespace Shared.Data.Persistence.Domain
+{
+    public interface IDataContext : IContext { }
+}
