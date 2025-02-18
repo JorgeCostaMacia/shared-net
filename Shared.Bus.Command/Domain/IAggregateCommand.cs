864109b71eec41878c380f44ac05fc3d59e1212f
JorgeCostaMacia <costamaciajorge@gmail.com>
2025-02-18 13:23:24 +0100

    UPDATE

--- Shared.Bus.Command/Domain/IAggregateCommand.cs
+++ Shared.Bus.Command/Domain/IAggregateCommand.cs
@@ -4,8 +4,6 @@ namespace Shared.Bus.Command.Domain;
 
 public abstract record IAggregateCommand : Message.Domain.IAggregateMessage, ICommand
 {
-    public Guid CorrelationId => AggregateId;
-
     protected IAggregateCommand(Guid aggregateId, DateTime aggregateOccurredAt) : base(aggregateId, aggregateOccurredAt) { }
     protected IAggregateCommand() : base() { }
 }
