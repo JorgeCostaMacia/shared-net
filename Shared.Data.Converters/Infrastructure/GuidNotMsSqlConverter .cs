cc6244394b2a61aabedac6168eea51627abac6ee
JorgeCostaMacia <jcosta@institutobernabeu.com>
2025-09-14 14:00:40 +0200

    UPDATE

---
+++ Shared.Data.Converters/Infrastructure/GuidNotMsSqlConverter .cs
@@ -0,0 +1,32 @@
+﻿using Shared.Service.Domain;
+#if !NET9_0 || !NET8_0|| !NET7_0|| !NET6_0
+using System;
+#endif
+
+namespace Shared.Data.Converters.Infrastructure
+{
+    public sealed class GuidInvertBytesConverter : IService
+    {
+        public static byte[] ConvertToBytes(Guid guid)
+        {
+            byte[] Value = guid.ToByteArray();
+
+            Array.Reverse(Value, 0, 4);
+            Array.Reverse(Value, 4, 2);
+            Array.Reverse(Value, 6, 2);
+
+            return Value;
+        }
+
+        public static Guid ConvertFromBytes(byte[] guid)
+        {
+            byte[] Value = (byte[])guid.Clone();
+
+            Array.Reverse(Value, 0, 4);
+            Array.Reverse(Value, 4, 2);
+            Array.Reverse(Value, 6, 2);
+
+            return new Guid(Value);
+        }
+    }
+}
