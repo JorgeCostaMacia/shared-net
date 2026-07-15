7504f4f250d114a1758381a1a0dac684af50670f
Jorge Costa Maciá <costamaciajorge@gmail.com>
2026-07-15 22:13:35 +0200

    test: cover the public forwarding ctor on ValueObject validation exceptions
    Assert the now-public nullable-metadata forwarding constructor applies explicit
    aggregateCode/type/httpCode, falls back to ValidationExceptionDefaults on null,
    and forwards the provided code through a derived exception.

--- test/JorgeCostaMacia.ValueObject.Tests/Domain/ValueObjectValidationExceptionHierarchyTests.cs
+++ test/JorgeCostaMacia.ValueObject.Tests/Domain/ValueObjectValidationExceptionHierarchyTests.cs
@@ -1,4 +1,5 @@
 using FluentValidation.Results;
+using JorgeCostaMacia.Exception.Domain;
 using JorgeCostaMacia.ValueObject.Domain;
 
 namespace JorgeCostaMacia.ValueObject.Tests.Domain;
@@ -31,5 +32,38 @@ public class ValueObjectValidationExceptionHierarchyTests
         DateTimeValueObjectValidationException local = new DateTimeValueObjectValidationException(new List<ValidationFailure>());
 
         Assert.NotEqual(local.AggregateCode, utc.AggregateCode);
+    }
+
+    [Fact]
+    public void PublicForwardingCtor_WithExplicitMetadata_AppliesIt()
+    {
+        Guid code = new Guid("0195f3d2-1a4b-7c8e-9f01-a2b3c4d5e6f7");
+        BoolValueObjectValidationException exception = new BoolValueObjectValidationException(
+            Guid.NewGuid(), "My.Custom.Type", code, 422, DateTime.UtcNow, "boom", null, new List<ValidationFailure>());
+
+        Assert.Equal(code, exception.AggregateCode);
+        Assert.Equal("My.Custom.Type", exception.AggregateType);
+        Assert.Equal(422, exception.AggregateHttpCode);
+    }
+
+    [Fact]
+    public void PublicForwardingCtor_WithNullMetadata_FallsBackToDefaults()
+    {
+        BoolValueObjectValidationException exception = new BoolValueObjectValidationException(
+            null, null, null, null, null, null, null, new List<ValidationFailure>());
+
+        Assert.Equal(ValidationExceptionDefaults.AGGREGATE_CODE, exception.AggregateCode);
+        Assert.Equal(ValidationExceptionDefaults.AGGREGATE_TYPE, exception.AggregateType);
+        Assert.Equal(ValidationExceptionDefaults.AGGREGATE_HTTP_CODE, exception.AggregateHttpCode);
+    }
+
+    [Fact]
+    public void DerivedException_PublicForwardingCtor_AppliesProvidedCode()
+    {
+        Guid code = new Guid("0195f3d2-2b5c-7d9f-a012-b3c4d5e6f708");
+        GroupByValueObjectValidationException exception = new GroupByValueObjectValidationException(
+            null, null, code, null, null, null, null, new List<ValidationFailure>());
+
+        Assert.Equal(code, exception.AggregateCode);
     }
 }
